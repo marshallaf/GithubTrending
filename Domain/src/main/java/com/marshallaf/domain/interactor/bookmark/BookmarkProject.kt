cf7040c70b596c9444829752e41244362049c843
Andrew Marshall <marshall.a.f@gmail.com>
2018-07-05 20:29:25 -0500

    Add bookmark project use case

---
+++ Domain/src/main/java/com/marshallaf/domain/interactor/bookmark/BookmarkProject.kt
@@ -0,0 +1,26 @@
+package com.marshallaf.domain.interactor.bookmark
+
+import com.marshallaf.domain.executor.PostExecutionThread
+import com.marshallaf.domain.interactor.CompletableUseCase
+import com.marshallaf.domain.repository.ProjectsRepository
+import io.reactivex.Completable
+import javax.inject.Inject
+
+class BookmarkProject @Inject constructor(
+    private val projectsRepository: ProjectsRepository,
+    postExecutionThread: PostExecutionThread
+) : CompletableUseCase<BookmarkProject.Params>(postExecutionThread) {
+
+  data class Params constructor(val projectId: String) {
+    companion object {
+      fun forProject(projectId: String): Params {
+        return Params(projectId)
+      }
+    }
+  }
+
+  override fun buildUseCaseCompletable(params: Params?): Completable {
+    if (params == null) throw IllegalArgumentException("params cannot be null")
+    return projectsRepository.bookmarkProject(params.projectId)
+  }
+}
