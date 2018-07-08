7780933ebe1d9d8dfd7d1a3b610648aaba95781a
Andrew Marshall <marshall.a.f@gmail.com>
2018-07-08 11:28:48 -0500

    Create data store interface

---
+++ Data/src/main/java/com/marshallaf/data/repository/ProjectsDataStore.kt
@@ -0,0 +1,16 @@
+package com.marshallaf.data.repository
+
+import com.marshallaf.data.model.ProjectEntity
+import io.reactivex.Completable
+import io.reactivex.Observable
+
+interface ProjectsDataStore {
+
+  fun getProjects(): Observable<List<ProjectEntity>>
+
+  fun getBookmarkedProjects(): Observable<List<ProjectEntity>>
+
+  fun setProjectBookmarked(projectId: String): Completable
+
+  fun setProjectUnbookmarked(projectId: String): Completable
+}
