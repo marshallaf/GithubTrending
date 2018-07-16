cf0169a1381ea5da968a590c863e59c49d64de64
Andrew Marshall <marshall.a.f@gmail.com>
2018-07-15 22:09:27 -0500

    Add tests for ProjectsRemoteImpl

--- Remote/src/main/java/com/marshallaf/remote/mapper/ProjectsResponseModelMapper.kt
+++ Remote/src/main/java/com/marshallaf/remote/mapper/ProjectsResponseModelMapper.kt
@@ -2,14 +2,13 @@ package com.marshallaf.remote.mapper
 
 import com.marshallaf.data.model.ProjectEntity
 import com.marshallaf.remote.model.ProjectModel
-import com.marshallaf.remote.model.ProjectsResponseModel
 
-class ProjectsResponseModelMapper: ModelMapper<ProjectModel, ProjectEntity> {
+class ProjectsResponseModelMapper : ModelMapper<ProjectModel, ProjectEntity> {
 
   override fun mapFromModel(model: ProjectModel): ProjectEntity {
     return ProjectEntity(model.id, model.name, model.fullName,
-    model.starCount.toString(), model.dateCreated, model.owner.ownerName,
-    model.owner.ownerAvatar)
+        model.starCount.toString(), model.dateCreated, model.owner.ownerName,
+        model.owner.ownerAvatar)
   }
 
 }
