42986a0e60ae7602c5d64e684f07768b292ba171
Andrew Marshall <marshall.a.f@gmail.com>
2018-07-16 23:18:51 -0500

    Create project cache impl

--- cache/src/main/java/com/marshallaf/cache/dao/ConfigurationDao.kt
+++ cache/src/main/java/com/marshallaf/cache/dao/ConfigurationDao.kt
@@ -6,12 +6,13 @@ import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import com.marshallaf.cache.db.ConfigurationConstants
 import com.marshallaf.cache.model.Configuration
+import io.reactivex.Flowable
 
 @Dao
 abstract class ConfigurationDao {
 
   @Query(ConfigurationConstants.QUERY_CONFIG)
-  abstract fun getConfiguration(): Configuration
+  abstract fun getConfiguration(): Flowable<Configuration>
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
   abstract fun insertConfiguration(configuration: Configuration)
