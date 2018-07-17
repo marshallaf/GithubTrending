97065990adc8e4d7dd03783b08a392e3df0111f6
Andrew Marshall <marshall.a.f@gmail.com>
2018-07-16 22:56:54 -0500

    Add mapper

---
+++ cache/src/main/java/com/marshallaf/cache/mapper/CachedProjectMapper.kt
@@ -0,0 +1,17 @@
+package com.marshallaf.cache.mapper
+
+import com.marshallaf.cache.model.CachedProject
+import com.marshallaf.data.model.ProjectEntity
+
+class CachedProjectMapper: CacheMapper<CachedProject, ProjectEntity> {
+
+  override fun mapFromCached(cached: CachedProject): ProjectEntity {
+    return ProjectEntity(cached.id, cached.name, cached.fullName, cached.starCount,
+        cached.dateCreated, cached.ownerName, cached.ownerAvatar, cached.isBookmarked)
+  }
+
+  override fun mapToCached(entity: ProjectEntity): CachedProject {
+    return CachedProject(entity.id, entity.name, entity.fullName, entity.starCount,
+        entity.dateCreated, entity.ownerName, entity.ownerAvatar, entity.isBookmarked)
+  }
+}
