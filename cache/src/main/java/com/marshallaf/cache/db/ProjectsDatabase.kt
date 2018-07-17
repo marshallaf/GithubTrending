20e305c2f63023a0b3f718dc1fd1dfbb0b97b73f
Andrew Marshall <marshall.a.f@gmail.com>
2018-07-16 22:33:12 -0500

    Create database class

---
+++ cache/src/main/java/com/marshallaf/cache/db/ProjectsDatabase.kt
@@ -0,0 +1,30 @@
+package com.marshallaf.cache.db
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import com.marshallaf.cache.model.CachedProject
+import javax.inject.Inject
+
+@Database(entities = arrayOf(CachedProject::class), version = 1)
+abstract class ProjectsDatabase @Inject constructor(): RoomDatabase() {
+
+  abstract fun cachedProjectsDao(): CachedProjectsDao
+
+  private var INSTANCE: ProjectsDatabase? = null
+  private val lock = Any()
+
+  fun getInstance(context: Context): ProjectsDatabase {
+    if (INSTANCE == null) {
+      synchronized(lock) {
+        if (INSTANCE == null) {
+          INSTANCE = Room.databaseBuilder(context.applicationContext, ProjectsDatabase::class.java, "projects.db")
+              .build()
+        }
+        return INSTANCE as ProjectsDatabase
+      }
+    }
+    return INSTANCE as ProjectsDatabase
+  }
+}
